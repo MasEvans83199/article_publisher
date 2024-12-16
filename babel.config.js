0da94a81b72bf0eebac137dddc737845ad822aa4
Mason Evans <masevans83199@gmail.com>
2024-12-16 12:20:50 -0600

    babel

--- babel.config.js
+++ babel.config.js
@@ -60,6 +60,12 @@ module.exports = function(api) {
         }
       ],
       [
+        '@babel/plugin-transform-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
         '@babel/plugin-proposal-private-property-in-object',
         {
           loose: true
